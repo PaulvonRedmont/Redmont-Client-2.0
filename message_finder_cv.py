94f44e5baab27e0977d11c5a9feda4b9aceacbcb
PaulvonRedmont <146851640+PaulvonRedmont@users.noreply.github.com>
2024-07-08 19:56:08 -0500

    Update message_finder_cv.py

--- message_finder_cv.py
+++ message_finder_cv.py
@@ -4,13 +4,11 @@ import os
 
 def find_and_crop_bubbles(target_image_path, output_folder):
     # Load the target image
-    target_image = cv2.imread(target_image_path, cv2.IMREAD_UNCHANGED)
-    if target_image.shape[2] != 4:
-        raise ValueError("The target image must be in RGBA format to retain color information accurately.")
-    # Convert the image to BGR (discarding the alpha channel for edge detection)
-    bgr_image = cv2.cvtColor(target_image, cv2.COLOR_BGRA2BGR)
+    target_image = cv2.imread(target_image_path)
+    if target_image is None:
+        raise ValueError("Could not load the image. Please check the image path.")
     # Convert the image to grayscale
-    gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(target_image, cv2.COLOR_BGR2GRAY)
     # Apply GaussianBlur to reduce noise and improve edge detection
     blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
     # Apply Canny edge detection
@@ -34,9 +32,8 @@ def find_and_crop_bubbles(target_image_path, output_folder):
         output_path = os.path.join(output_folder, f'cropped_box_{count}.png')
         cv2.imwrite(output_path, cropped_box)
         count += 1
-
 # Usage
 downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
-target_image_path = r"C:\Users\paule\Desktop\Redmont-Client-main\screenshots\Screenshots for OCR\local_screenshot1720183179.569418.png"  # Replace with the path to your target image file
+target_image_path = r"C:\Users\paule\Desktop\Redmont-Client-main\Redmont Client Screenshots\cropped_screenshot_2024-03-30_17-07-50.png"  # Replace with the path to your target image file
 
 find_and_crop_bubbles(target_image_path, downloads_folder)
