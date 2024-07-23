1c98343ec51120fe454fd92a380fe81b76fb34af
PaulvonRedmont <146851640+PaulvonRedmont@users.noreply.github.com>
2024-07-23 16:22:54 -0500

    Update client_logic_backend.py

--- client_logic_backend.py
+++ client_logic_backend.py
@@ -1,1 +1,17 @@
+# Alexa responses
+blacksmith_response_1 = "okay.Do you have time to enhance an item? Should I improve your sword, armor, or quarters?"
+blacksmith_response_1_1 = "you got it.Can you fit me in for an upgrade?Should I upgrade your sword, armor, or quarters?"
+blakcsmith_response_1_2 = "sure.Do you have time to enhance an item?Want me to empower your sword, armor, or quarters?"
+blacksmith_response_1_3 = "ok. Greetings Giovanni! Can I empower your sword, armor, or quarters?"
 
+
+
+# Client states
+yes = "yes"
+blacksmith = "blacksmith"
+
+# create a dictionary for searching alexa's responses 
+
+cient_logic_dictionary = [
+    "blacksmith", blacksmith_response_1, blacksmith_response_1_1, blakcsmith_response_1_2, blacksmith_response_1_3
+]
